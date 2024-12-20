9bf387a22c2fea935fc1d6efe17611a632c73e9e
skynet <chu_liang_han@hotmail.com>
2024-12-20 22:15:17 +0800

    update app and embed, requirements file.

--- gcs_chat_app/app.py
+++ gcs_chat_app/app.py
@@ -2,31 +2,33 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import VertexAIEmbeddings
-from langchain.vectorstores.pgvector import PGVector
+from langchain_community.embeddings import VertexAIEmbeddings
+from langchain_community.vectorstores.pgvector import PGVector
 from langchain.chains import RetrievalQA
-from langchain.llms import VertexAI
+from langchain_google_vertexai import VertexAI
+
 REQUESTS_PER_MINUTE = 10
+
 # Initialize session_state if it's not already defined
 if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "How can Ihelp you?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "How can I help you?"}]
     st.title("📝 Q&A PDF with VertexAI")
 
 with st.spinner('Wait for it...'):
     llm = VertexAI(
-    model_name='text-bison@001',
-    max_output_tokens=256,
-    temperature=0.1,
-    top_p=0.8,top_k=40,
-    verbose=True,
+        model_name='text-embedding-004',
+        max_output_tokens=256,
+        temperature=0.1,
+        top_p=0.8,top_k=40,
+        verbose=True,
     )
     embeddings = VertexAIEmbeddings(requests_per_minute=REQUESTS_PER_MINUTE)
-    CONNECTION_STRING ="postgresql+psycopg2://postgres:a49>rIc@10.14.48.3:5432/vector-db"
+    CONNECTION_STRING ="postgresql+psycopg2://postgres:P{vLX90{]{Q39$ZA@10.128.128.3:5432/vector-db"
     COLLECTION_NAME = 'test_collection'
     db = PGVector.from_existing_index(
-    embedding=embeddings,
-    connection_string=CONNECTION_STRING,
-    collection_name=COLLECTION_NAME,
+        embedding=embeddings,
+        connection_string=CONNECTION_STRING,
+        collection_name=COLLECTION_NAME,
     )
     st.success('Chat is ready')
 
--- gcs_embed/embed.py
+++ gcs_embed/embed.py
@@ -1,39 +1,42 @@
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains import RetrievalQA
 #from langchain.vectorstores import Chroma
-from langchain.vectorstores.pgvector import PGVector
-from langchain.document_loaders import GCSDirectoryLoader
-from langchain.llms import VertexAI
-from langchain.embeddings import VertexAIEmbeddings
+from langchain_community.vectorstores.pgvector import PGVector
+from langchain_community.document_loaders import GCSDirectoryLoader
+from langchain_google_vertexai import VertexAI
+from langchain_community.embeddings import VertexAIEmbeddings
 from flask import Flask
 # added as it is necessary
 import os
 import unstructured
 import sys
+
 llm = VertexAI(
-    model_name='text-bison@001',
+    model_name='text-embedding-004',
     max_output_tokens=256,
     temperature=0.1,
     top_p=0.8,top_k=40,
     verbose=True,
-    project_id='chat-app-vertex-ai'
+    project_id='ctg-rag-model-001'
 )
 app = Flask(__name__)
+
+
 @app.route('/', methods = ['POST', 'GET'])
 def embed():
     REQUESTS_PER_MINUTE = 100
     embedding = VertexAIEmbeddings(requests_per_minute=REQUESTS_PER_MINUTE)
     # load document
-    loader = GCSDirectoryLoader(project_name="chat-app-vertex-ai",
-            bucket="test-training-data-uqq25-i07l3j38sua09911mybb3lw2db")
+    loader = GCSDirectoryLoader(project_name="ctg-rag-model-001",
+            bucket="ctg-rag-model-bucket-001")
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     texts = text_splitter.split_documents(documents)
     print(len(texts))
     embeddings = VertexAIEmbeddings(requests_per_minute=REQUESTS_PER_MINUTE)
 
-    CONNECTION_STRING = "postgresql+psycopg2://postgres:a49>krIc@10.14.48.3:5432/vector-db"
+    CONNECTION_STRING = "postgresql+psycopg2://postgres:P{vLX90{]{Q39$ZA@10.128.128.3:5432/vector-db"
     COLLECTION_NAME = 'test_collection'
     db = PGVector.from_documents(
         embedding=embeddings,
@@ -42,6 +45,8 @@ def embed():
         collection_name=COLLECTION_NAME,
     )
     return 'done embed'
+
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
